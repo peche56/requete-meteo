2725587cb04b1b947787da228f0368f18c0dff5e
peche56 <c.filleul56@gmail.com>
2017-03-08 13:12:44 +0100

    integration jquery

--- requete.js
+++ requete.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
 
 var city = $('input:text').val();
-
+ $list = $('ul');
 
   $('#weather').click(function() {
-    console.log(city);
+    // console.log(city);
+
+    $list.append('<li>' + (city) + '</li>');
 
 
-  });
+$.get( "api.openweathermap.org/data/2.5/weather?q={city name}", function( city ) {
+  $( "body" )
+    $list.append( '<li>' "Weather conditions : " + weather.id '</li>') // John
+    $list.append( '<li>' "Weather description: " + weather.main '</li>') //  2pm
+    $list.append( '<li>' "Temperature: " + main.temp '</li>');
+}, "json" );
 });
-
-
-
-
-
-$.get( "test.php", function( data ) {
-alert( "Data Loaded: " + data );
 });
